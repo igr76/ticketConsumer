35b965d1b46799c077cba28e3d815bca8b8b2704
igr76 <gritsuk766@gmail.com>
2023-12-29 20:57:26 +1000

    start

---
+++ src/main/java/com/example/ticketconcumer/TicketConcumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketconcumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketConcumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketConcumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ticketconcumer/TicketConcumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketconcumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketConcumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
